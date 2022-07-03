3758d1bbf0ced957a4340158f7a2413aef1c610e
unknown <suraj8660911981@gmail.com>
2022-07-03 20:13:05 +0530

    add gitignore,procfile and upadte ports

--- app.js
+++ app.js
@@ -352,6 +352,11 @@ app.get("/logout",function(req,res){
     res.render("logout");
 })
 
-app.listen(3000,function(req, res){
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(req, res){
     console.log("Server is running on port 3000");
 });
